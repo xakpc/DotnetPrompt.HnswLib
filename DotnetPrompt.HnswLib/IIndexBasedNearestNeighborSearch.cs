38657d4710ebe84d7c66d6c260d6bb71835c1853
Pavel Osadchuk <xakz.pc@gmail.com>
2023-04-12 20:56:14 +0200

    chore: major clean up

---
+++ DotnetPrompt.HnswLib/IIndexBasedNearestNeighborSearch.cs
@@ -0,0 +1,14 @@
+﻿namespace DotnetPrompt.HnswLib;
+
+public interface IIndexBasedNearestNeighborSearch
+{
+    void AddPoint(ReadOnlySpan<float> datapoint, int label);
+
+    IEnumerable<(float distance, int label)> SearchKnn(float[] queryData, int k, Func<int, bool>? isIdAllowed = null)
+    {
+        var ret = SearchKnnCloserFirst(queryData, k, isIdAllowed); // Here "searchKnn" returns the result in the order of closest first
+        return ret.Reverse();
+    }
+
+    IEnumerable<(float distance, int label)> SearchKnnCloserFirst(float[] queryData, int k, Func<int, bool>? isIdAllowed = null);
+}
